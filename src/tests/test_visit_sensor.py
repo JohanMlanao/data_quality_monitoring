6a3b6547e98044a2ac135b88538f44fe1b900114
JohanMlanao <johan.mlanao@gmail.com>
2025-04-30 16:44:34 +0200

    Add test without malfunction

--- src/tests/test_visit_sensor.py
+++ src/tests/test_visit_sensor.py
@@ -6,7 +6,7 @@ from datetime import date
 class TestVisitSensor(unittest.TestCase):
 
     def test_monday_open(self):
-        visit_sensor = VisitSensor(1200, 300)
+        visit_sensor = VisitSensor(avg_visit=1200, std_visit=300)
         visit_count = visit_sensor.simulate_visit_count(
             date(year=2025, month=4, day=7), business_hour=12
         )
@@ -14,7 +14,7 @@ class TestVisitSensor(unittest.TestCase):
         self.assertFalse(visit_count == -1)
 
     def test_monday_closed(self):
-        visit_sensor = VisitSensor(1200, 300)
+        visit_sensor = VisitSensor(avg_visit=1200, std_visit=300)
         visit_count = visit_sensor.simulate_visit_count(
             date(year=2025, month=4, day=7), business_hour=22
         )
@@ -22,7 +22,7 @@ class TestVisitSensor(unittest.TestCase):
         self.assertFalse(visit_count != -1)
 
     def test_tuesday_open(self):
-        visit_sensor = VisitSensor(1200, 300)
+        visit_sensor = VisitSensor(avg_visit=1200, std_visit=300)
         visit_count = visit_sensor.simulate_visit_count(
             date(year=2025, month=4, day=8), business_hour=12
         )
@@ -30,15 +30,15 @@ class TestVisitSensor(unittest.TestCase):
         self.assertFalse(visit_count == -1)
 
     def test_tuesday_closed(self):
-        visit_sensor = VisitSensor(1200, 300)
+        visit_sensor = VisitSensor(avg_visit=1200, std_visit=300)
         visit_count = visit_sensor.simulate_visit_count(
-            date(2025, 4, 8), business_hour=22
+            date(year=2025, month=4, day=12), business_hour=22
         )
 
         self.assertFalse(visit_count != -1)
 
     def test_wednesday_open(self):
-        visit_sensor = VisitSensor(1200, 300)
+        visit_sensor = VisitSensor(avg_visit=1200, std_visit=300)
         visit_count = visit_sensor.simulate_visit_count(
             date(year=2025, month=4, day=9), business_hour=12
         )
@@ -46,15 +46,15 @@ class TestVisitSensor(unittest.TestCase):
         self.assertFalse(visit_count == -1)
 
     def test_wednesday_closed(self):
-        visit_sensor = VisitSensor(1200, 300)
+        visit_sensor = VisitSensor(avg_visit=1200, std_visit=300)
         visit_count = visit_sensor.simulate_visit_count(
-            date(2025, 4, 9), business_hour=22
+            date(year=2025, month=4, day=12), business_hour=22
         )
 
         self.assertFalse(visit_count != -1)
 
     def test_thursday_open(self):
-        visit_sensor = VisitSensor(1200, 300)
+        visit_sensor = VisitSensor(avg_visit=1200, std_visit=300)
         visit_count = visit_sensor.simulate_visit_count(
             date(year=2025, month=4, day=10), business_hour=12
         )
@@ -62,15 +62,15 @@ class TestVisitSensor(unittest.TestCase):
         self.assertFalse(visit_count == -1)
 
     def test_thursday_closed(self):
-        visit_sensor = VisitSensor(1200, 300)
+        visit_sensor = VisitSensor(avg_visit=1200, std_visit=300)
         visit_count = visit_sensor.simulate_visit_count(
-            date(2025, 4, 10), business_hour=22
+            date(year=2025, month=4, day=12), business_hour=22
         )
 
         self.assertFalse(visit_count != -1)
 
     def test_friday_open(self):
-        visit_sensor = VisitSensor(1200, 300)
+        visit_sensor = VisitSensor(avg_visit=1200, std_visit=300)
         visit_count = visit_sensor.simulate_visit_count(
             date(year=2025, month=4, day=11), business_hour=12
         )
@@ -78,15 +78,15 @@ class TestVisitSensor(unittest.TestCase):
         self.assertFalse(visit_count == -1)
 
     def test_friday_closed(self):
-        visit_sensor = VisitSensor(1200, 300)
+        visit_sensor = VisitSensor(avg_visit=1200, std_visit=300)
         visit_count = visit_sensor.simulate_visit_count(
-            date(2025, 4, 11), business_hour=22
+            date(year=2025, month=4, day=12), business_hour=22
         )
 
         self.assertFalse(visit_count != -1)
 
     def test_saturday_open(self):
-        visit_sensor = VisitSensor(1200, 300)
+        visit_sensor = VisitSensor(avg_visit=1200, std_visit=300)
         visit_count = visit_sensor.simulate_visit_count(
             date(year=2025, month=4, day=12), business_hour=12
         )
@@ -94,32 +94,38 @@ class TestVisitSensor(unittest.TestCase):
         self.assertFalse(visit_count == -1)
 
     def test_saturday_closed(self):
-        visit_sensor = VisitSensor(1200, 300)
+        visit_sensor = VisitSensor(avg_visit=1200, std_visit=300)
         visit_count = visit_sensor.simulate_visit_count(
-            date(2025, 4, 12), business_hour=22
+            date(year=2025, month=4, day=12), business_hour=22
         )
 
         self.assertFalse(visit_count != -1)
 
     def test_sunday_closed(self):
-        visit_sensor = VisitSensor(1200, 300)
+        visit_sensor = VisitSensor(avg_visit=1200, std_visit=300)
         visit_count = visit_sensor.simulate_visit_count(
-            date(2025, 4, 13), business_hour=22
+            date(year=2025, month=4, day=12), business_hour=22
         )
 
         self.assertFalse(visit_count != -1)
 
     def test_with_break(self):
-        visit_sensor = VisitSensor(1200, 300, 10)
-        visit_count = visit_sensor.get_visit_count(date(2025, 4, 12), 12)
+        visit_sensor = VisitSensor(avg_visit=1200, std_visit=300, perc_break=10)
+        visit_count = visit_sensor.get_visit_count(date(year=2025, month=4, day=12), business_hour=12)
 
         self.assertEqual(visit_count,0)
 
     def test_with_malfunction(self):
-        visit_sensor = VisitSensor(1200, 300, )
-        visit_count = visit_sensor.get_visit_count(date(2025, 4, 1), 12)
+        visit_sensor = VisitSensor(avg_visit=1200, std_visit=300, perc_malfunction=123)
+        visit_count = visit_sensor.get_visit_count(date(year=2025, month=4, day=12), business_hour=12)
 
-        self.assertEqual(visit_count, 17.0)
+        self.assertEqual(visit_count, 19.0)
+
+    def test_without_malfunction(self):
+        visit_sensor = VisitSensor(avg_visit=1200, std_visit=300, perc_malfunction=0)
+        visit_count = visit_sensor.get_visit_count(date(year=2025, month=4, day=12), business_hour=12)
+
+        self.assertEqual(visit_count, 96)
 
 if __name__ == '__main__':
     unittest.main()
