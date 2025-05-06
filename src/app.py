f904899e46b54329ffd7c44da1f9a9c8c9d29e57
JohanMlanao <johan.mlanao@gmail.com>
2025-05-06 18:39:18 +0200

    Add sensor_id conditions and change visit_count calculation

--- src/app.py
+++ src/app.py
@@ -11,10 +11,18 @@ app = FastAPI()
 
 
 @app.get("/")
-def visit(store_name: str, year: int, month: int, day: int) -> JSONResponse:
+def visit(
+    store_name: str, year: int, month: int, day: int, sensor_id: int | None = None
+) -> JSONResponse:
     # If the store is not in the dictionary
     if not (store_name in store_dict.keys()):
         return JSONResponse(status_code=404, content="Store Not Found")
+
+    # Check the value of sensor_id
+    if sensor_id and (sensor_id > 7 or sensor_id < 0):
+        return JSONResponse(
+            status_code=404, content="Sensor_id should be between 0 and 7"
+        )
 
     # Check the year
     if year < 2020:
@@ -31,9 +39,18 @@ def visit(store_name: str, year: int, month: int, day: int) -> JSONResponse:
     if date.today() < requested_date:
         return JSONResponse(status_code=404, content="Choose a date in the past")
 
-    return JSONResponse(
-        status_code=200,
-        content=store_dict[f"{store_name}"].get_visit_count(
-            date(year=year, month=month, day=day),
-        ),
-    )
+    # If no sensor choose return the visit for the whole store
+    if sensor_id is None:
+        visit_count = store_dict[store_name].get_all_traffic(
+            date(year=year, month=month, day=day)
+        )
+    else:
+        visit_count = store_dict[store_name].get_sensor_traffic(
+            sensor_id, date(year=year, month=month, day=day)
+        )
+    if visit_count < 0:
+        return JSONResponse(
+            status_code=404, content="The store was closed try another date"
+        )
+
+    return JSONResponse(status_code=200, content=visit_count)
