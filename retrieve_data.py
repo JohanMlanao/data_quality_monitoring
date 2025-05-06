dd660b3fc433bd6ad2bb4fae4335ed4d862636c8
JohanMlanao <johan.mlanao@gmail.com>
2025-05-06 22:24:17 +0200

    Add input and sensor selection

--- retrieve_data.py
+++ retrieve_data.py
@@ -1,30 +1,34 @@
-import sys
-from datetime import date, timedelta
+from datetime import date, datetime, timedelta
 
 import pandas as pd
 import requests
 
 
 def get_data(
-    business_name: str,
-    business_year: int,
-    business_month: int,
-    business_day: int,
+    business_parameter: str,
 ) -> int:
     r = requests.get(
         "https://data-quality-monitoring-j9nq.onrender.com",
-        params=f"store_name={business_name}&year={business_year}&month={business_month}&day={business_day}",
+        params=business_parameter,
     )
     return r.text
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        store_name, day, month, year = [v for v in sys.argv[1].split("-")]
+    store_name = input("Please enter the store location: ")
+    date_input = input("Please enter a date in the format DD-MM-YYYY: ")
+    date_format = "%d-%m-%Y"
+    business_date = datetime.strptime(date_input, date_format).date()
+    sensor_id = input(
+        "To view sensor traffic, enter a number from 1 to 8, or press Enter to view all traffic: "
+    )
+    init_date = business_date
+    if sensor_id and (0 < int(sensor_id) < 8):
+        sensor_id = int(sensor_id)
+        parameter = f"store_name={store_name}&year={init_date.year}&month={init_date.month}&day={init_date.day}&sensor_id={sensor_id}"
     else:
-        store_name, day, month, year = ["Bordeaux", 1, 5, 2025]
-
-    init_date = date(year=int(year), month=int(month), day=int(day))
+        print("Sensor ID not selected or is invalid; returning all traffic by default.")
+        parameter = f"store_name={store_name}&year={init_date.year}&month={init_date.month}&day={init_date.day}"
     init_hour = 0
     data = []
     while init_date < date.today():
@@ -35,12 +39,7 @@ if __name__ == "__main__":
         if init_hour < 8 or init_hour > 19:
             visit_count = -1
         else:
-            visit_count = get_data(
-                business_name=store_name,
-                business_year=init_date.year,
-                business_month=init_date.month,
-                business_day=init_date.day,
-            )
+            visit_count = get_data(business_parameter=parameter)
         row = {
             "store_name": store_name,
             "visit_count": visit_count,
@@ -55,5 +54,11 @@ if __name__ == "__main__":
 
     # Example: print first 2 rows of each group
     for (year, month), group_df in grouped.items():
-        filename = f"data/raw/data_{year}_{month:02}.csv"
+        # filename = f"data/raw/data_{store_name}_{year}_{month:02}.csv"
+        if sensor_id and (sensor_id < 8 and sensor_id > 0):
+            filename = (
+                f"data/raw/data_{store_name}_{year}_{month:02}_sensor{sensor_id}.csv"
+            )
+        else:
+            filename = f"data/raw/data_{store_name}_{year}_{month:02}.csv"
         group_df.to_csv(filename, index=False)
