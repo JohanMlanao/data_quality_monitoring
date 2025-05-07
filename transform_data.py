9a34de8200d8a9feb540b6db5eaf5afe5e17fd4d
JohanMlanao <johan.mlanao@gmail.com>
2025-05-07 19:39:17 +0200

    Add groupby function and dataframe cleaning

--- transform_data.py
+++ transform_data.py
@@ -7,3 +7,17 @@ path = "data/raw/"
 csv_files = glob.glob(os.path.join(path, "*.csv"))
 df = pd.concat(map(pd.read_csv, csv_files))
 print(df)
+
+df_day = (
+    df.groupby(by=["store_location", "year", "month", "day"])["visit_count"]
+    .sum()
+    .reset_index()
+)
+
+print(df_day)
+
+df_day.dropna(inplace=True)
+
+regular_values = (df_day['visit_count'] > 0)
+
+print(df_day[regular_values])
