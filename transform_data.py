6f6566b319ee5d026e2d66c36d866bdbf5640914
JohanMlanao <johan.mlanao@gmail.com>
2025-05-08 12:19:33 +0200

    Add day_of_week and moving_average columns

--- transform_data.py
+++ transform_data.py
@@ -6,18 +6,28 @@ import pandas as pd
 path = "data/raw/"
 csv_files = glob.glob(os.path.join(path, "*.csv"))
 df = pd.concat(map(pd.read_csv, csv_files))
+df["date"] = pd.to_datetime(dict(year=df.year, month=df.month, day=df.day))
 print(df)
 
-df_day = (
-    df.groupby(by=["store_location", "year", "month", "day"])["visit_count"]
-    .sum()
-    .reset_index()
-)
-
-print(df_day)
+df_day = df.groupby(by=["store_location", "date"])["visit_count"].sum().reset_index()
 
 df_day.dropna(inplace=True)
+df_day.query("visit_count > 0", inplace=True)
+df_day["day_of_week"] = df_day["date"].dt.dayofweek
 
-regular_values = (df_day['visit_count'] > 0)
-
-print(df_day[regular_values])
+df_day["moving_avg_last_4_weekday"] = (
+    df_day.groupby(by=["day_of_week"])["visit_count"]
+    .rolling(window=4, min_periods=1)
+    .mean()
+    .reset_index(level=0, drop=True)
+)
+df_day = df_day[
+    [
+        "store_location",
+        "date",
+        "day_of_week",
+        "visit_count",
+        "moving_avg_last_4_weekday",
+    ]
+]
+print(df_day.sort_values(by="date"))
