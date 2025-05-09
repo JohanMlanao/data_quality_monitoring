cb833ec13f79b0a9397ef879524fc1b08ee0e5c8
JohanMlanao <johan.mlanao@gmail.com>
2025-05-09 17:14:33 +0200

    Add sensor column

--- transform_data.py
+++ transform_data.py
@@ -4,26 +4,37 @@ import os
 import pandas as pd
 
 path = "data/raw/"
-csv_files = glob.glob(os.path.join(path, "*.csv"))
-df = pd.concat(map(pd.read_csv, csv_files))
+all_sensors_data = []
+for i in range(1, 4):
+    csv_files = glob.glob(os.path.join(path, f"*_sensor{i}.csv"))
+    sensor_data = pd.concat(map(pd.read_csv, csv_files))
+    sensor_data["sensor_id"] = i
+    all_sensors_data.append(sensor_data)
+df = pd.concat(all_sensors_data)
 df["date"] = pd.to_datetime(dict(year=df.year, month=df.month, day=df.day))
 print(df)
 
-df_day = df.groupby(by=["store_location", "date"])["visit_count"].sum().reset_index()
+df_day = (
+    df.groupby(by=["store_location", "sensor_id", "date"])["visit_count"]
+    .sum()
+    .reset_index()
+)
 
 df_day.dropna(inplace=True)
 df_day.query("visit_count > 0", inplace=True)
 df_day["day_of_week"] = df_day["date"].dt.dayofweek
 
 df_day["moving_avg_4"] = (
-    df_day.groupby(by=["day_of_week"])["visit_count"]
+    df_day.groupby(by=["day_of_week", "sensor_id"])["visit_count"]
     .rolling(window=4, min_periods=1)
     .mean()
-    .reset_index(level=0, drop=True)
+    .reset_index(level=[0, 1], drop=True)
 )
+
 df_day = df_day[
     [
         "store_location",
+        "sensor_id",
         "date",
         "day_of_week",
         "visit_count",
