6314c1637607f27931a00b9049952532e08fc583
JohanMlanao <johan.mlanao@gmail.com>
2025-05-14 19:15:10 +0200

    First dag to launch extract_data.py and transform_data.py every month

---
+++ automated_etl.py
@@ -0,0 +1,25 @@
+import datetime
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="data_quality_monitoring",
+    schedule="0 8 1 * *",
+    catchup=False,
+    start_date=datetime.datetime(2025,5,1),
+    dagrun_timeout=datetime.timedelta(minutes=60),
+) as dag:
+    first_task = BashOperator(
+        task_id="first_task",
+        bash_command="python ~/data_quality_monitoring/extract_data.py"
+    )
+
+    second_task = BashOperator(
+        task_id="second_task",
+        bash_command="python ~/data_quality_monitoring/transform_data.py",
+    )
+    first_task >> second_task
+
+if __name__ == "__main__":
+    dag.test()
