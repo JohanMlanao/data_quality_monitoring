92770f24ee8bbcd8749559f50fc6f1dcf681cbd1
jmlanao <johan3333@hotmail.fr>
2025-05-15 10:58:18 +0200

    Split manual and automated extraction in two files

--- extract_data.py
+++ extract_data.py
@@ -1,8 +1,8 @@
 from datetime import date, datetime, timedelta
+from itertools import product
 
 import pandas as pd
 import requests
-import sys
 
 
 def get_data(business_parameter: str) -> tuple[str, int]:
@@ -17,27 +17,15 @@ def get_data(business_parameter: str) -> tuple[str, int]:
     return r.text, r.status_code
 
 
-def get_user_inputs() -> tuple[str, date, str]:
-    if len(sys.argv) > 1:
-        store_location, sensor_id, day, month, year = [
-            v for v in sys.argv[1].split("-")
-        ]
-        business_date = date(year=int(year), month=int(month), day=int(day))
-    else:
-        store_location = "Bordeaux"
-        sensor_id = "all_sensors"
-        business_date = date.today()
-
-    return store_location, business_date, sensor_id
-
-
-def is_valid_sensor(sensor_id: str) -> bool:
-    """Checks whether the sensor_id is a valid number from 0 to 4."""
-    return sensor_id.isdigit() and 0 <= int(sensor_id) < 4
+def get_input() -> tuple[list[str], date, list[int]]:
+    store_locations = ["Lille", "Paris", "Lyon", "Bordeaux", "Marseille"]
+    sensors = [0, 1, 2, 3]
+    business_date = date.today()
+    return store_locations, business_date, sensors
 
 
 def collect_traffic_data(
-    store_location: str, sensor_id: str, start_date: date
+    store_locations: list[str], sensors: list[int], start_date: date
 ) -> list[dict]:
     """
     Iterates from start_date to today, hour by hour, collecting traffic data.
@@ -45,109 +33,69 @@ def collect_traffic_data(
     """
     data = []
     current_hour = 0
+    # Replace the day with 1 to get the first day of the month
+    first_day = start_date.replace(day=1)
+    last_day_previous_month = first_day - timedelta(days=1)
+    # Retrieve the first day of the previous month
+    current_date = last_day_previous_month.replace(day=1)
+    condition = current_date <= last_day_previous_month
+    while current_date <= last_day_previous_month:
+        current_hour += 1
+        if current_hour == 24:
+            current_date += timedelta(days=1)
+            current_hour = 0
+        if current_date > last_day_previous_month:
+            break
 
-    if sensor_id == "all_sensors" and start_date == date.today():
-        # Replace the day with 1 to get the first day of the month
-        first_day = start_date.replace(day=1)
-        last_day_previous_month = first_day - timedelta(days=1)
-        # Retrieve the first day of the previous month
-        current_date = last_day_previous_month.replace(day=1)
-        condition = current_date <= last_day_previous_month
-        while current_date <= last_day_previous_month:
-            current_hour += 1
-            if current_hour == 24:
-                current_date += timedelta(days=1)
-                current_hour = 0
-            if current_date > last_day_previous_month:
-                break
-
-            if current_hour < 8 or current_hour > 19:
-                for sensor in range(4):
-                    sensor_row = {
-                        "store_location": store_location,
-                        "sensor_id": sensor,
-                        "visit_count": 0,
-                        "hour": current_hour,
-                        "day": current_date.day,
-                        "month": current_date.month,
-                        "year": current_date.year,
-                    }
-                    data.append(sensor_row)
-            else:
-                for sensor in range(4):
-                    params = (
-                        f"store_location={store_location}"
-                        f"&year={current_date.year}&month={current_date.month}&day={current_date.day}&sensor_id={sensor}"
-                    )
-                    visit_count, status = get_data(business_parameter=params)
-                    sensor_row = {
-                        "store_location": store_location,
-                        "sensor_id": sensor,
-                        "visit_count": visit_count,
-                        "hour": current_hour,
-                        "day": current_date.day,
-                        "month": current_date.month,
-                        "year": current_date.year,
-                    }
-                    data.append(sensor_row)
-    else:
-        current_date = start_date
-        while current_date <= date.today():
-            if current_hour < 8 or current_hour > 19:
-                visit_count = 0
-            elif is_valid_sensor(sensor_id):
+        if current_hour < 8 or current_hour > 19:
+            for store_location, sensor in product(store_locations, sensors):
+                sensor_row = {
+                    "store_location": store_location,
+                    "sensor_id": sensor,
+                    "visit_count": 0,
+                    "hour": current_hour,
+                    "day": current_date.day,
+                    "month": current_date.month,
+                    "year": current_date.year,
+                }
+                data.append(sensor_row)
+        else:
+            for store_location, sensor in product(store_locations, sensors):
                 params = (
                     f"store_location={store_location}"
-                    f"&year={current_date.year}&month={current_date.month}&day={current_date.day}&sensor_id={sensor_id}"
+                    f"&year={current_date.year}&month={current_date.month}&day={current_date.day}&sensor_id={sensor}"
                 )
                 visit_count, status = get_data(business_parameter=params)
-            else:
-                params = (
-                    f"store_location={store_location}"
-                    f"&year={current_date.year}&month={current_date.month}&day={current_date.day}"
-                )
-                visit_count, status = get_data(business_parameter=params)
-
-                if status == 404:
-                    print("\nThe specified store location could not be found.")
-                    return []
-
-            row = {
-                "store_location": store_location,
-                "visit_count": visit_count,
-                "hour": current_hour,
-                "day": current_date.day,
-                "month": current_date.month,
-                "year": current_date.year,
-            }
-            data.append(row)
-
+                sensor_row = {
+                    "store_location": store_location,
+                    "sensor_id": sensor,
+                    "visit_count": visit_count,
+                    "hour": current_hour,
+                    "day": current_date.day,
+                    "month": current_date.month,
+                    "year": current_date.year,
+                }
+                data.append(sensor_row)
     return data
 
 
-def save_data_by_month(data: list[dict], store_location: str, sensor_id: str):
+def save_data_by_month(data: list[dict]):
     """Groups data by month and saves each group as a separate CSV file."""
     df = pd.DataFrame(data)
     grouped = dict(tuple(df.groupby(["year", "month"])))
 
     for (year, month), group_df in grouped.items():
-        if is_valid_sensor(sensor_id):
-            filename = f"~/data_quality_monitoring/data/raw/data_{store_location}_{year}_{month:02}_sensor{sensor_id}.csv"
-        else:
-            filename = f"~/data_quality_monitoring/data/raw/data_{store_location}_{year}_{month:02}.csv"
+        filename = f"~/data_quality_monitoring/data/raw/data_{year}_{month:02}.csv"
         group_df.to_csv(filename, index=False)
 
 
 def main():
-    store_location, business_date, sensor_id = get_user_inputs()
-
-    if sensor_id and not is_valid_sensor(sensor_id):
-        print("Sensor ID not selected or is invalid; returning all traffic by default.")
+    store_location, business_date, sensor_id = get_input()
 
     data = collect_traffic_data(store_location, sensor_id, business_date)
 
     if data:
-        save_data_by_month(data, store_location, sensor_id)
+        save_data_by_month(data)
 
 
 if __name__ == "__main__":
