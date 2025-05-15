080f473397d3be0621abe90e8f787c9735d6fcf4
jmlanao <johan3333@hotmail.fr>
2025-05-15 10:00:33 +0200

    Correcting input in each tests

--- tests/test_store.py
+++ tests/test_store.py
@@ -7,21 +7,21 @@ from src.store import StoreSensor
 class TestStoreSensor(unittest.TestCase):
 
     def test_get_sensor_traffic(self):
-        store = StoreSensor(name="Lille", avg_visit=1500, std_visit=300)
+        store = StoreSensor(location="Lille", avg_visit=1500, std_visit=300)
         traffic = store.get_sensor_traffic(
             sensor_id=1, business_date=date(year=2025, month=4, day=2)
         )
-        self.assertEqual(traffic, 3)
+        self.assertEqual(traffic, 10)
 
     def test_get_all_traffic(self):
-        store = StoreSensor(name="Lille", avg_visit=1500, std_visit=300)
+        store = StoreSensor(location="Lille", avg_visit=1500, std_visit=300)
         traffic = store.get_all_traffic(business_date=date(year=2025, month=4, day=2))
-        self.assertEqual(traffic, 27)
+        self.assertEqual(traffic, 149)
 
     def test_store_closed_sunday(self):
-        store = StoreSensor(name="Lille", avg_visit=1500, std_visit=300)
+        store = StoreSensor(location="Lille", avg_visit=1500, std_visit=300)
         traffic = store.get_all_traffic(business_date=date(year=2025, month=5, day=4))
-        self.assertEqual(traffic, -1)
+        self.assertEqual(traffic, 0)
 
 
 if __name__ == "__main__":
