174c888f9ba2bbe1482005ad1592ecf30bcd7963
jmlanao <johan3333@hotmail.fr>
2025-05-16 11:54:50 +0200

    Add bolding to text tools

--- homepage.py
+++ homepage.py
@@ -17,7 +17,7 @@ st.markdown("### 🎯 Project Goal")
 st.write("To proactively monitor and maintain data quality through an automated and interactive pipeline.")
 
 # Tools & Technologies
-st.markdown("## 🛠️ Tools & Technologies Used")
+st.markdown("### 🛠️ Tools & Technologies Used")
 cols = st.columns(6)
 tools = {
     "FastAPI": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png",
@@ -29,32 +29,31 @@ tools = {
 }
 
 for col, logo_url in zip(cols, tools.values()):
-    col.image(logo_url, width=80)
-st.markdown("---")
+    col.image(logo_url, width=100)
 
 # Data Pipeline
-st.markdown("## 🔄 Data Pipeline")
+st.markdown("### 🔄 Data Pipeline")
 
 st.markdown("""
 This project simulates a real-world data quality monitoring workflow using a complete data pipeline. Below is an overview of each step in the pipeline:
 
 1. **Simulated Company Database (FastAPI)**  
-   The project starts with a FastAPI backend designed to simulate a company’s data source. It mimics how a real application would expose its data via an API.
+   The project starts with a `FastAPI` backend designed to simulate a company’s data source. It mimics how a real application would expose its data via an API.
 
 2. **Data Extraction (Python + Pandas)**  
    Python scripts interact with the API to fetch data. These scripts use `requests` and `pandas` to retrieve and parse the data into structured dataframes.
 
-3. **Transformation and Storage (Pandas + DuckDB)**  
+3. **Transformation and Storage (Pandas)**  
    The extracted data is cleaned and transformed using `pandas`. Final datasets are saved as efficient Parquet files.
 
 4. **ETL Automation (Airflow)**  
-   The entire Extract-Transform-Load process is orchestrated and scheduled using Apache Airflow, ensuring repeatability and automation.
+   The entire Extract-Transform-Load process is orchestrated and scheduled using `Apache Airflow`, ensuring repeatability and automation.
    
 5. **Database Creation (DuckDB)**  
-   After automation, the Parquet data is loaded into a local analytical database using DuckDB, enabling efficient querying and integration with the dashboard.
+   After automation, the Parquet data is loaded into a local analytical database using `DuckDB`, enabling efficient querying and integration with the dashboard.
 
 6. **Interactive Visualization (Streamlit App)**  
-   Finally, the processed data is loaded into a Streamlit app where users can explore data quality insights via dynamic charts and dashboards.
+   Finally, the processed data is loaded into a `Streamlit` app where users can explore data quality insights via dynamic charts and dashboards.
 
 ⚠️ Due to slow response times from Render, manual extraction is occasionally used to speed up the visualization process during development.
 """)
